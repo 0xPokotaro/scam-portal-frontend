3204bc7ee7b019658febedc86e848b3d4fdeca33
0xpokotaro <kentarou_masuda@ruckplus-tech.io>
2024-03-07 01:00:51 +0900

    fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -16,7 +16,7 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <p className="font-bold text-inherit">暗号資産の詐欺情報サイト</p>
+            <p className="font-bold text-inherit">暗号資産の情報サイト</p>
           </NextLink>
         </NavbarBrand>
       </NavbarContent>
