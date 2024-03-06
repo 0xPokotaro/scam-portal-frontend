df2f19a7493cb5f7c8837381d1ec87ec5a4a474e
0xpokotaro <kentarou_masuda@ruckplus-tech.io>
2024-03-06 11:26:06 +0900

    fixed

--- config/projects.ts
+++ config/projects.ts
@@ -1,9 +1,19 @@
 export const projects: {
   id: number
   name: string
+  categories: string[]
+  chains: string[]
 }[] = [
   {
     id: 1,
     name: '0XLSD',
+    categories: ['DeFi'],
+    chains: ['Ethereum']
+  },
+  {
+    id: 2,
+    name: 'KoiTrading',
+    categories: ['DEX'],
+    chains: ['Ethereum', 'BSC']
   },
 ]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import { title, subtitle } from '@/components/primitives'
 import DefaultLayout from '@/layouts/default'
-import { Button, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from '@nextui-org/react'
+import { Button, Chip, Table, TableHeader, TableColumn, TableBody, TableRow, TableCell } from '@nextui-org/react'
 import { projects } from '@/config/projects'
 
 export default function IndexPage() {
@@ -19,17 +19,33 @@ export default function IndexPage() {
         </div>
 
         <div className="mt-8">
-          <Table>
+          <Table isStriped>
             <TableHeader>
               <TableColumn>プロジェクト名</TableColumn>
-              <TableColumn>内容</TableColumn>
+              <TableColumn>カテゴリー</TableColumn>
+              <TableColumn>チェーン</TableColumn>
               <TableColumn>詳細</TableColumn>
             </TableHeader>
             <TableBody>
-              {projects.map((project) => (
+              {projects.reverse().map((project) => (
                 <TableRow key={project.id}>
-                  <TableCell>{project.name}</TableCell>
-                  <TableCell>ほげほげ</TableCell>
+                  <TableCell>
+                    <span className="font-bold">{project.name}</span>
+                  </TableCell>
+                  <TableCell>
+                    {project.categories.map((category) => (
+                      <Chip key={category} variant="bordered" className="mr-1">
+                        {category}
+                      </Chip>
+                    ))}
+                  </TableCell>
+                  <TableCell>
+                    {project.chains.map((chain) => (
+                      <Chip key={chain} variant="bordered" className="mr-1">
+                        {chain}
+                      </Chip>
+                    ))}
+                  </TableCell>
                   <TableCell>
                     <Button color="primary" size="sm">詳細</Button>
                   </TableCell>
