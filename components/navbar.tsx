7151a2988907d38239733aba669e4ea62d8a173f
0xpokotaro <kentarou_masuda@ruckplus-tech.io>
2024-03-06 23:30:47 +0900

    fixed

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,47 +1,16 @@
 import {
-  Kbd,
   Link,
-  Input,
   Navbar as NextUINavbar,
   NavbarContent,
-  NavbarMenu,
-  NavbarMenuToggle,
   NavbarBrand,
   NavbarItem,
-  NavbarMenuItem,
 } from '@nextui-org/react'
-
-import { link as linkStyles } from '@nextui-org/theme'
-
 import { siteConfig } from '@/config/site'
 import NextLink from 'next/link'
-import clsx from 'clsx'
-
 import { ThemeSwitch } from '@/components/theme-switch'
-import { TwitterIcon, GithubIcon, SearchIcon } from '@/components/icons'
+import { TwitterIcon } from '@/components/icons'
 
 export const Navbar = () => {
-  const searchInput = (
-    <Input
-      aria-label="Search"
-      classNames={{
-        inputWrapper: 'bg-default-100',
-        input: 'text-sm',
-      }}
-      endContent={
-        <Kbd className="hidden lg:inline-block" keys={['command']}>
-          K
-        </Kbd>
-      }
-      labelPlacement="outside"
-      placeholder="Search..."
-      startContent={
-        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0" />
-      }
-      type="search"
-    />
-  )
-
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
@@ -50,24 +19,6 @@ export const Navbar = () => {
             <p className="font-bold text-inherit">暗号資産の詐欺情報サイト</p>
           </NextLink>
         </NavbarBrand>
-        {/*
-        <div className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: 'foreground' }),
-                  'data-[active=true]:text-primary data-[active=true]:font-medium'
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </div>
-         */}
       </NavbarContent>
 
       <NavbarContent
@@ -87,31 +38,7 @@ export const Navbar = () => {
           <TwitterIcon className="text-default-500" />
         </Link>
         <ThemeSwitch />
-        <NavbarMenuToggle />
       </NavbarContent>
-
-      <NavbarMenu>
-        {searchInput}
-        <div className="mx-4 mt-2 flex flex-col gap-2">
-          {siteConfig.navMenuItems.map((item, index) => (
-            <NavbarMenuItem key={`${item}-${index}`}>
-              <Link
-                color={
-                  index === 2
-                    ? 'primary'
-                    : index === siteConfig.navMenuItems.length - 1
-                      ? 'danger'
-                      : 'foreground'
-                }
-                href="#"
-                size="lg"
-              >
-                {item.label}
-              </Link>
-            </NavbarMenuItem>
-          ))}
-        </div>
-      </NavbarMenu>
     </NextUINavbar>
   )
 }
